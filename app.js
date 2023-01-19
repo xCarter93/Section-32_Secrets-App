102de805479aae2004e9ef24fe11143caf73b529
xCarter93 <xcarter93@gmail.com>
2023-01-19 09:22:11 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,19 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(
+	bodyParser.urlencoded({
+		extended: true,
+	})
+);
+
+app.listen(3000, () => {
+	console.log("Server is running");
+});
