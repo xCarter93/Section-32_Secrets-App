8e9d530932cf0136c540594a4bb5347d7cb25090
xCarter93 <xcarter93@gmail.com>
2023-01-19 10:07:45 -0500

    Added level 1 authentication: checking if user exists in Database and rendering secrets page

--- app.js
+++ app.js
@@ -20,6 +20,13 @@ const db = "secretsDB";
 const mongoURL = `mongodb+srv://xcarter93:${data.password}@cluster0.jwhx2lt.mongodb.net/${db}?retryWrites=true&w=majority`;
 mongoose.connect(mongoURL);
 
+const userSchema = new mongoose.Schema({
+	email: String,
+	password: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
 app.get("/", (req, res) => {
 	res.render("home");
 });
@@ -32,12 +39,36 @@ app.get("/register", (req, res) => {
 	res.render("register");
 });
 
-app.get("/secrets", (req, res) => {
-	res.render("secrets");
+app.post("/register", (req, res) => {
+	const user = new User({
+		email: req.body.username,
+		password: req.body.password,
+	});
+
+	user.save((err) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render("secrets");
+		}
+	});
 });
 
-app.get("/submit", (req, res) => {
-	res.render("submit");
+app.post("/login", (req, res) => {
+	User.findOne(
+		{ email: req.body.username, password: req.body.password },
+		(err, foundUser) => {
+			if (!err) {
+				if (foundUser) {
+					res.render("secrets");
+				} else {
+					console.log("User not found, please register.");
+				}
+			} else {
+				console.log(err);
+			}
+		}
+	);
 });
 
 app.listen(3000, () => {
