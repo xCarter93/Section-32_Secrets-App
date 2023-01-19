2cdcfeeb210ee5bd1d4464ea43d201cb6e97b478
xCarter93 <xcarter93@gmail.com>
2023-01-19 09:35:13 -0500

    Added Mongoose to project

--- app.js
+++ app.js
@@ -3,9 +3,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
+const data = require("/Users/patrickcarter/VisualStudioCodeProjects/HTML-CSS-JS/Complete 2022 Web Development Bootcamp/mongoPassword.json");
 
 const app = express();
-
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(
@@ -13,6 +14,31 @@ app.use(
 		extended: true,
 	})
 );
+
+mongoose.set("strictQuery", false);
+const db = "secretsDB";
+const mongoURL = `mongodb+srv://xcarter93:${data.password}@cluster0.jwhx2lt.mongodb.net/${db}?retryWrites=true&w=majority`;
+mongoose.connect(mongoURL);
+
+app.get("/", (req, res) => {
+	res.render("home");
+});
+
+app.get("/login", (req, res) => {
+	res.render("login");
+});
+
+app.get("/register", (req, res) => {
+	res.render("register");
+});
+
+app.get("/secrets", (req, res) => {
+	res.render("secrets");
+});
+
+app.get("/submit", (req, res) => {
+	res.render("submit");
+});
 
 app.listen(3000, () => {
 	console.log("Server is running");
